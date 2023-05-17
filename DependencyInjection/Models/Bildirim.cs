ca3b736623ed7b55bd21f783a449d6c14abd7f46
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-17 16:51:43 +0300

    Dependency-Injection-with-.Net-Core-

---
+++ DependencyInjection/Models/Bildirim.cs
@@ -0,0 +1,20 @@
+﻿namespace DependencyInjection.Models
+{
+    public class Bildirim
+    {
+        public void Yap()
+        {
+            EPostaBildirim ePostaBildirim = new EPostaBildirim();
+            ePostaBildirim.Gonder();
+
+        }
+    }
+
+    public class EPostaBildirim
+    {
+        public void Gonder()
+        {
+            System.Console.WriteLine("Eposta gönderildi");
+        }
+    }
+}
